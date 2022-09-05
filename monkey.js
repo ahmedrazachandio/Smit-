3234a1ce9e9caec67e2fadcc4a3c9d3c84571777
AhmedRaza <86877851+ahmedrazachandio@users.noreply.github.com>
2022-09-05 11:00:44 +0500

    class code

---
+++ monkey.js
@@ -0,0 +1,11 @@
+var githubUrl1 = "https://github.com/ahmedrazachandio";
+
+function githubUrl() {
+    window.open(githubUrl1, "GitHub Url","height=400, width=300");
+    
+}
+
+
+function closeGithubUrl(){
+    monkeyWindow.close();
+}
